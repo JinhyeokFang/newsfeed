ba6b0f70f6ea07cbe5981dae396312978f640795
jinhyeokfang <jinhyeokfang@gmail.com>
2023-03-02 21:57:29 +0900

    refactor: made event emitter injectable

--- src/common/event.ts
+++ src/common/event.ts
@@ -1,11 +1,26 @@
 import EventEmitter2 from 'eventemitter2';
+import { injectable } from 'inversify';
 
-export const Event = new EventEmitter2({
-  wildcard: false,
-  delimiter: '.',
-  newListener: false,
-  removeListener: false,
-  maxListeners: 10,
-  verboseMemoryLeak: false,
-  ignoreErrors: false,
-});
+@injectable()
+export class Event {
+  private static emitter = new EventEmitter2({
+    wildcard: false,
+    delimiter: '.',
+    newListener: false,
+    removeListener: false,
+    maxListeners: 10,
+    verboseMemoryLeak: false,
+    ignoreErrors: false,
+  });
+
+  on(
+    eventName: string,
+    callback: (eventData: Record<string, unknown>) => void,
+  ): void {
+    Event.emitter.on(eventName, callback);
+  }
+
+  emit(eventName: string, eventData: Record<string, unknown>) {
+    Event.emitter.emit(eventName, eventData);
+  }
+}
