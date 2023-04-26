06eca5d9847a87bfd761c5c879db5ca5736037f3
jinhyeokfang <jinhyeokfang@gmail.com>
2023-04-26 22:07:42 +0900

    refactor: remove AccountInmemoryRepository

--- src/account/business/account.service.test.ts
+++ src/account/business/account.service.test.ts
@@ -1,22 +1,46 @@
 import 'reflect-metadata';
 import { Event } from '../../common/event/event';
-import { AccountInmemoryRepository } from '../infrastructure/account.inmemory.repository';
+import { UserId } from '../../account/domain/user-id';
+import { Account } from '../domain/account';
+import { AccountRepository } from '../domain/account.repository';
+import { AccountMysqlRepository } from '../infrastructure/account.mysql.repository';
 import { AccountService } from './account.service';
 
 describe('AccountService', () => {
+  let accountRepository: AccountRepository;
   let accountService: AccountService;
 
+  const email = 'email';
+  const password = 'password';
+  const name = 'name';
+  const userId = new UserId('userid');
+  const account = Account.create({
+    email,
+    password,
+    name,
+    id: userId,
+  });
+
   beforeEach(() => {
-    const accountRepository = new AccountInmemoryRepository();
+    accountRepository = new AccountMysqlRepository(null);
     accountService = new AccountService(accountRepository, new Event());
+
+    jest
+      .spyOn(accountRepository, 'findOneById')
+      .mockImplementation(async (id) => {
+        return id === userId ? account : null;
+      });
+    jest
+      .spyOn(accountRepository, 'findOneByEmail')
+      .mockImplementation(async (userEmail) => {
+        return userEmail === email ? account : null;
+      });
+    jest.spyOn(accountRepository, 'save').mockImplementation(async () => {
+      return null;
+    });
   });
 
   it('AccountService.register()', async () => {
-    // given
-    const email = 'email';
-    const password = 'password';
-    const name = 'name';
-
     // when
     await accountService.register(email, password, name);
 
