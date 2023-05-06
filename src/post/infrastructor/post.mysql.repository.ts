f8195a0f176e90de61ff0593bbea293984bdf6b6
jinhyeokfang <jinhyeokfang@gmail.com>
2023-05-06 22:25:27 +0900

    feat: postMysqlRepository supports post.comments

--- src/post/infrastructor/post.mysql.repository.ts
+++ src/post/infrastructor/post.mysql.repository.ts
@@ -5,6 +5,8 @@ import { Post } from '../domain/post';
 import { PostRepository } from '../domain/post.repository';
 import PostData from './post.data-mapper';
 import PostLikeData from './post-like.data-mapper';
+import { Comment } from '../domain/comment';
+import CommentData from './comment.data-mapper';
 
 @injectable()
 export class PostMysqlRepository implements PostRepository {
@@ -17,6 +19,7 @@ export class PostMysqlRepository implements PostRepository {
     const pool = this.dataSource.createPool();
     const createPostSql = `INSERT IGNORE INTO posts (id, title, authorUserId, content) VALUES (?, ?, ?, ?)`;
     await this.saveLikes(post.likes.slice(), post.id);
+    await this.saveComments(post.comments.slice(), post.id);
     await pool.execute(createPostSql, [
       post.id,
       post.title,
@@ -33,14 +36,19 @@ export class PostMysqlRepository implements PostRepository {
     const postDatas: PostData[] = result[0] as PostData[];
 
     return await Promise.all(
-      postDatas.map(async (postData) =>
-        new Post.builder(new UserId(postData.authorUserId))
+      postDatas.map(async (postData) => {
+        const [likes, comments] = await Promise.all([
+          this.getLikesByPostId(postData.id),
+          this.getCommentsByPostId(postData.id),
+        ]);
+        return new Post.builder(new UserId(postData.authorUserId))
           .setId(postData.id)
           .setTitle(postData.title)
           .setContent(postData.content)
-          .setLikes(await this.getLikesByPostId(postData.id))
-          .build(),
-      ),
+          .setComments(comments)
+          .setLikes(likes)
+          .build();
+      }),
     );
   }
 
@@ -54,12 +62,17 @@ export class PostMysqlRepository implements PostRepository {
     }
 
     const postData = postDatas[0];
-    const likes = await this.getLikesByPostId(id);
+    const [likes, comments] = await Promise.all([
+      this.getLikesByPostId(id),
+      this.getCommentsByPostId(id),
+    ]);
+
     return new Post.builder(new UserId(postData.authorUserId))
       .setId(id)
       .setTitle(postData.title)
       .setContent(postData.content)
       .setLikes(likes)
+      .setComments(comments)
       .build();
   }
 
@@ -71,6 +84,20 @@ export class PostMysqlRepository implements PostRepository {
     return likeDatas.map((likeData) => new UserId(likeData.userId));
   }
 
+  private async getCommentsByPostId(postId: number): Promise<Comment[]> {
+    const sql = `SELECT * FROM comments WHERE postId = ?`;
+    const result = await this.dataSource.createPool().query(sql, [postId]);
+    const commentDatas: CommentData[] = result[0] as CommentData[];
+
+    return commentDatas.map(
+      (commentDatas) =>
+        new Comment(
+          new UserId(commentDatas.authorUserId),
+          commentDatas.comment,
+        ),
+    );
+  }
+
   private async saveLikes(likes: UserId[], postId: number): Promise<void> {
     const pool = this.dataSource.createPool();
     const removeSql = `DELETE FROM post_like_list WHERE postId = ?`;
@@ -79,7 +106,19 @@ export class PostMysqlRepository implements PostRepository {
       const sql = `INSERT INTO post_like_list (userId, postId) VALUES (?, ?)`;
       await pool.execute(sql, [like.toString(), postId]);
     });
-    return;
+  }
+
+  private async saveComments(
+    comments: Comment[],
+    postId: number,
+  ): Promise<void> {
+    const pool = this.dataSource.createPool();
+    const removeSql = `DELETE FROM comments WHERE postId = ?`;
+    await pool.execute(removeSql, [postId]);
+    comments.forEach(async (comment) => {
+      const sql = `INSERT INTO comments (authorUserId, postId, comments) VALUES (?, ?, ?)`;
+      await pool.execute(sql, [postId, comment.userId, comment.content]);
+    });
   }
 
   async deleteOneById(id: number): Promise<void> {
